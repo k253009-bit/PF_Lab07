eef93180c55526ab1841a6d8466fa8043e799dc1
Saad Ahmed <k253009@nu.edu.pk>
2025-10-24 00:08:30 +0500

    Update Task8.c

--- Task8.c
+++ Task8.c
@@ -4,15 +4,16 @@ int main(){
     char str[100];
     int i;
 
-    printf("Enter characters (non-alphabets will be accepted): ");
-    scanf("%[^A-Za-z]", str);  
+    printf("Enter characters: ");
+    scanf("%[^\n]", str); 
 
-    printf("\nYou entered the following non-alphabetic characters:\n");
-
-    for(i=0; str[i]!='\0'; i++){
-        printf("%c", str[i]);
+    printf("\nNon-alphabetic characters: ");
+    for(i=0; str[i] != '\0'; i++){
+        if(!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i]<= 'z')))
+            printf("%c", str[i]);
     }
-
     printf("\n");
     return 0;
 }
+
+
